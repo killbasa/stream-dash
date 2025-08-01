f6455233bbd4c4c4633d21905c665166f218c60e
killbasa <git@killbasa.com>
2025-07-31 21:03:24 -0400

    prisma

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from './generated/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
 import { env } from '$env/dynamic/private';
 
 const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
