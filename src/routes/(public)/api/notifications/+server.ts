6d81d0ed3f91941cd3f193c585209818eef81c24
killbasa <git@killbasa.com>
2025-08-03 13:05:33 -0400

    more logging

--- src/routes/(public)/api/notifications/+server.ts
+++ src/routes/(public)/api/notifications/+server.ts
@@ -2,9 +2,22 @@ import { ok } from '$lib/server/api';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async (event) => {
-	const data = await event.request.json();
+	if (event.request.headers.get('Content-Type') !== 'application/json') {
+		return new Response('Invalid content type', { status: 400 });
+	}
 
-	console.log(data);
+	if (!event.request.body) {
+		return new Response('No body provided', { status: 400 });
+	}
 
-	return ok();
+	// Parse the JSON body
+	try {
+		const data = await event.request.json();
+		console.log(data);
+
+		return ok();
+	} catch (err) {
+		console.log('Error parsing JSON:', err);
+		return new Response('Invalid JSON body', { status: 400 });
+	}
 };
