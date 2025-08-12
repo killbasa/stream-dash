b0b5f8156017aa0d51aecc1e1567a1c7a56b166b
killbasa <git@killbasa.com>
2025-08-12 18:41:57 -0400

    add meta tags

--- src/routes/(public)/player/[id]/+page.svelte
+++ src/routes/(public)/player/[id]/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import { page } from '$app/state';
 
 	let { data }: PageProps = $props();
 </script>
 
 <svelte:head>
 	<title>{data.name}</title>
+
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={page.url.href} />
+	<meta property="og:title" content={data.name} />
 </svelte:head>
 
 <iframe
