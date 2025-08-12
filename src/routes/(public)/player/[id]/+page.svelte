a249192743807ec7d6742cccf975d279c0dda975
killbasa <git@killbasa.com>
2025-08-12 18:58:46 -0400

    dont autoplay by default

--- src/routes/(public)/player/[id]/+page.svelte
+++ src/routes/(public)/player/[id]/+page.svelte
@@ -3,6 +3,15 @@
 	import { page } from '$app/state';
 
 	let { data }: PageProps = $props();
+
+	const iframeUrl = new URL(
+		`/${data.id}/iframe`,
+		`https://${data.customerId}.cloudflarestream.com`,
+	);
+
+	if (page.url.searchParams.get('embed') === 'true') {
+		iframeUrl.searchParams.set('autoplay', '1');
+	}
 </script>
 
 <svelte:head>
@@ -15,7 +24,7 @@
 
 <iframe
 	title="Live Input Player"
-	src="https://{data.customerId}.cloudflarestream.com/{data.id}/iframe?autoplay=1"
+	src={iframeUrl.href}
 	style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
 	allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
 ></iframe>
